f0c12808c6aef7b8f1dd16f02c78ba42a5fcbbf5
Eren Ertem <mail.erenertem@gmail.com>
2025-08-17 15:32:39 +0300

    conditions

---
+++ conditions.py
@@ -0,0 +1,21 @@
+# if statements do not need parentheses or braces
+
+n = 1
+if n > 2:
+  n -= 1
+  print(n)
+elif n == 2:
+  n *= 2
+  print(n)
+else:
+  n += 2
+
+# parentheses needed for multi-line conditions
+# and -> &&
+# or -> ||
+
+n, m = 1, 2
+
+if((n > 2 and
+    n != m) or n == m):
+  n += 1
